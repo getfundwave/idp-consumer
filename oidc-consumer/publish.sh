3fef9d6040558a1f7b4c9bf53059ae9395a184f5
giriparus <parusgiri@gmail.com>
2024-06-28 17:12:22 +0530

    Consider npmrc of parent directory

--- oidc-consumer/publish.sh
+++ oidc-consumer/publish.sh
@@ -5,10 +5,13 @@ GITLAB_TOKEN=${2:-}
 
 [[ -n $version ]] || { echo "usage: bash publish.sh <version> <optional GITLAB_TOKEN>" && exit 1; }
 
+mv ../.npmrc ../.npmrc.cp
+
 npm ci
 npm publish --tag $version --access public
 
 if [[ -n $GITLAB_TOKEN ]]; then
+
 
 echo @fundwave:registry=https://gitlab.com/api/v4/projects/24877554/packages/npm/ >> ./.npmrc
 echo //gitlab.com/api/v4/projects/24877554/packages/npm/:_authToken="$GITLAB_TOKEN" >> ./.npmrc
@@ -16,3 +19,5 @@ npm publish --tag $version
 rm ./.npmrc
 
 fi
+
+mv ../.npmrc.cp ../.npmrc
