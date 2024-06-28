3447e1887b595e11dfd2b9082f3c20daa24eff87
giriparus <parusgiri@gmail.com>
2024-06-28 16:30:33 +0530

    add script to publish package manually

---
+++ oidc-consumer/publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+version=$1
+GITLAB_TOKEN=${2:-}
+
+[[ -n $version ]] || { echo "usage: bash publish.sh <version> <optional GITLAB_TOKEN>" && exit 1; }
+
+npm ci
+npm publish --tag $version --access public
+
+if [[ -n $GITLAB_TOKEN ]]; then
+
+echo @fundwave:registry=https://gitlab.com/api/v4/projects/24877554/packages/npm/ >> ./.npmrc
+echo //gitlab.com/api/v4/projects/24877554/packages/npm/:_authToken="$GITLAB_TOKEN" >> ./.npmrc
+npm publish --tag $version
+rm ./.npmrc
+
+fi
